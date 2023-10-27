5a038acd708514b698ba04b3b7251b095e5d7b83
ShreyaAkkalkot <149154485+ShreyaAkkalkot@users.noreply.github.com>
2023-10-27 14:54:00 +0530

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,23 @@
+#string concatenation
+#three ways concantenation
+#name="shreya"
+#print("Hello "+name)
+#print(f"Hello {name}")
+#print("hello {}".format(name))
+
+print("A day at Zoo MAdlibs")
+adjective1=input("Adjective:")
+noun1=input("Noun:")
+verb1=input("Verb:")
+adverb1=input("Adverb:")
+adjective2=input("Adjective:")
+noun2=input("Noun:")
+noun3=input("Noun:")
+adjective3=input("Adjective:")
+verb2=input("Verb:")
+adverb3=input("Adverb:")
+print(f"Today I went to zoo.I saw a(n) {adjective1} {noun1} jumping up and down in its tree.")
+print(f"He {verb1} {adverb1} through the large tunnel that led to its {adjective2} {noun2}.")
+print(f"I got some peanuts and passed them through the cage to a gigantic gray {noun3} towering above my head.")
+print(f"Feeding that animal made me hungry. I went to get a {adjective3} scoop of ice cream. It filled my stomach.")
+print(f"Afterwards I had to {verb2} {adverb3} to catch our bus.")
